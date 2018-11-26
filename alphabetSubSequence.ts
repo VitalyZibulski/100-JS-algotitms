28fed37e07362c0784cbfe186c1e4f57cbf8cc4d
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-26 18:09:04 +0300

    alphabetSubSequence

---
+++ alphabetSubSequence.ts
@@ -0,0 +1,23 @@
+function aplphabetSubsequence(s: string): boolean {
+    const chars: string [] = s.split('');
+    const charValues: number[] = [];
+
+    chars.forEach((char: string) => {
+        charValues.push(char.charCodeAt(0));
+    })
+
+    if(new Set(charValues).size !== charValues.length) {
+        return false;
+    }
+
+    for (let i = 0; i < charValues.length; i++) {
+        if(charValues[i] >= charValues[i+1]) {
+            return false
+        }
+        
+    }
+
+    return true;
+}
+
+console.log(aplphabetSubsequence('zab'));
